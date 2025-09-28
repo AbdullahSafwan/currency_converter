23a83475c8131e2bed76ce0ec7f6ae9927e47801
abdullahsafwan <abdullahsafwan768@gmail.com>
2025-09-28 15:06:51 +0500

    update script to fetch rate once

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 try {
-    let cny_rate = document.getElementById('cny');
-        let usd_rate = document.getElementById('usd');
+  let cny_rate = document.getElementById("cny");
+  let usd_rate = document.getElementById("usd");
+  let exchangeRate;
+  const getRate = async () => {
+    const data = await fetch(
+      "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json"
+    );
+    const json = await data.json();
+    const para = document.createElement("p");
+    const node = document.createTextNode(`1 USD = ${json.usd.cny} CNY`);
+    para.appendChild(node);
+    document.body.appendChild(para);
+    return json.usd.cny;
+  };
 
-        const rate = fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json')
-            .then(res => res.json())
-            .then(data => data.usd.cny);
+  (async () => {
+    exchangeRate = await getRate();
 
-        cny_rate.addEventListener('input', async function() {
-            let r = await rate;
-            usd.value = (this.value / r).toFixed(2);
-        });
+    cny_rate.addEventListener("input", function () {
+      usd.value = (this.value / exchangeRate).toFixed(2);
+    });
 
-        usd_rate.addEventListener('input', async function() {
-            let r = await rate;
-            cny.value = (this.value * r).toFixed(5);
-        });
+    usd_rate.addEventListener("input", function () {
+      cny.value = (this.value * exchangeRate).toFixed(5);
+    });
+  })();
+
+  cny_rate.addEventListener("input", function () {
+    usd.value = (this.value / exchangeRate).toFixed(2);
+  });
+
+  usd_rate.addEventListener("input", function () {
+    cny.value = (this.value * exchangeRate).toFixed(5);
+  });
 } catch (error) {
-    console.error
+  console.error;
 }
